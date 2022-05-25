a570a38d78a7e843b92dd2e145b4a7f6c2225d57
priyankabohra <93154248+priyankabohra@users.noreply.github.com>
2022-05-25 16:02:53 +0200

    Update EmployeeManagementApplicationTests.java

--- src/test/java/com/management/employee/EmployeeManagementApplicationTests.java
+++ src/test/java/com/management/employee/EmployeeManagementApplicationTests.java
@@ -4,10 +4,10 @@ package com.management.employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+ @SpringBootTest
 class EmployeeManagementApplicationTests {
 
 	void contextLoads() {
 	}
 
-}
+}. 
